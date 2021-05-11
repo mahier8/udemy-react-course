fe9af0f50d72c87596ac2195af8551f228478928
Mahier Sydow <mahier.sydow8@gmail.com>
2021-05-11 20:08:57 +0900

    Expense budget complete

--- 01-starting-setup/src/components/Expenses/DisplayExpense.js
+++ 01-starting-setup/src/components/Expenses/DisplayExpense.js
@@ -3,6 +3,7 @@ import './DisplayExpense.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const DisplayExpense = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -23,7 +24,8 @@ const DisplayExpense = (props) => {
                 <ExpensesFilter 
                 selected={filteredYear} 
                 onChangeFilter={filterChangeHandler}
-                />  
+                />
+                <ExpensesChart expenses={filteredExpenses} />  
                 <ExpensesList 
                 items={filteredExpenses}
                 />
